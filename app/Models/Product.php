e0771e84751b7771c752c27c27d14c04e08d9233
Amr Sobhy <76039186+amrsobhy2020@users.noreply.github.com>
2024-08-12 22:20:23 +0200

    UpdateProductTbl

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,6 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = "products";
-    protected $fillable = ['Id', 'name_ar', 'name_en', 'price', 'description_ar', 'description_en', 'image', 'created_at', 'updated_at'] ;
+    protected $fillable = ['name_ar', 'name_en', 'price', 'description_ar', 'description_en', 'image', 'created_at', 'updated_at'] ;
     protected $hidden = ['created_at','updated_at'] ;
 }
